87e196f08d1b73d54379f53f5f9a32f44f7daaad
Sangeeth-jay <sangeethjayawarna@gmail.com>
2025-09-27 09:53:55 +0530

    create loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import OTP from "./(auth)/otpauthorization/page";
+import LoadingUI from "@/components/ui/loading";
 
 export default function Home() {
   return (
     <>
       <main className="w-full h-dvh bg-background">
-        <div className="max-w-4xl mx-auto h-full">
-          <OTP/>
-        </div>
+        {/* <div className="max-w-4xl mx-auto h-full">
+          dashboard wrapper
+        </div> */}
+
+        <LoadingUI/>
       </main>
     </>
   );
---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,84 @@
+"use client";
+import React from "react";
+import logo from "@/assessts/hatch 1.png";
+import Image from "next/image";
+import { motion } from "motion/react";
+
+const containerVarients = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const squareLeftVariants = {
+  hidden: { x: -60, rotate: -30, opacity: 0 },
+  show: {
+    x: 0,
+    rotate: -12,
+    opacity: 1,
+    transition: { type: "spring" as const, stiffness: 120, damping: 15 },
+  },
+};
+
+const squareRightVariants = {
+  hidden: { x: 60, rotate: 30, opacity: 0 },
+  show: {
+    x: 0,
+    rotate: 12,
+    opacity: 1,
+    transition: { type: "spring" as const, stiffness: 120, damping: 15 },
+  },
+};
+
+const eggVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { duration: 0.5 },
+  },
+};
+
+const LoadingUI = () => {
+  return (
+    <>
+      <div className="w-full h-screen flex flex-col gap-8 items-center justify-center">
+        <motion.div
+          variants={containerVarients}
+          initial="hidden"
+          animate="show"
+          className="relative flex flex-row items-end justify-center"
+        >
+          <motion.div
+            variants={squareLeftVariants}
+            className="absolute w-36 h-36 bg-[var(--cgreen-1)] rounded-2xl -rotate-12 -translate-y-3 -translate-x-0.5 shadow-[0_0_3px_var(--cgreen-2)]"
+          ></motion.div>
+          <motion.div
+            variants={squareRightVariants}
+            className="absolute w-32 h-32 bg-[var(--cgreen-4)] rounded-2xl rotate-12 shadow-lg"
+          ></motion.div>
+          <motion.div
+            variants={eggVariants}
+            animate={{ rotate: [0, -5, 5, -5, 5, 0] }}
+            transition={{
+              duration: 1,
+              delay: 0.8,
+              repeat: Infinity,
+              repeatDelay: 2,
+              ease: "easeInOut",
+            }}
+            className="relative inline-block"
+            style={{ transformOrigin: "50% 100%" }}
+          >
+            <Image src={logo} alt="logo" />
+          </motion.div>
+        </motion.div>
+
+      </div>
+    </>
+  );
+};
+
+export default LoadingUI;
